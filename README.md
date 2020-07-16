e13c9a44743cffe5d9c5bce9838839593a216303
vladayson <vladaysin@gmail.com>
2020-07-16 12:05:38 +0300

    initial

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# yii2-rsvg
-Linux rsvg-convert wrapper
+Yii2 rsvg wrapper
+=====================
+Yii2 rsvg wrapper
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist vladayson/yii2-rsvg "*"
+```
+
+or add
+
+```
+"vladayson/yii2-rsvg": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, add it in your config file:
+
+```
+/config/web.php
+```
+
+Add rsvg component
+```
+'components' => [
+        ...
+        'rsvg' => [
+            'class' => vladayson\rsvg\Rsvg::class,
+        ],
+        ...
+]
+```
+
+And use it
+
+```php
+<?php Yii::$app->rsvg->format('<path_to_svg>/.svg', '<path_to_png>/.png', 'png') ?>```
---
+++ src/Rsvg.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace vladayson\rsvg;
+
+use yii\base\Component;
+
+/**
+ * Class Inkscape
+ * @package vladayson\Inkscape
+ */
+class Rsvg extends Component
+{
+    /**
+     * @var null|int
+     */
+    private $width = null;
+
+    /**
+     * @var null|int
+     */
+    private $height = null;
+
+    /**
+     * @param $svgFilename
+     * @param $outputFilename
+     * @param $format 'png', 'pdf', 'ps', 'svg', 'xml', 'recording'
+     *
+     * @return string|null
+     */
+    public function convert($svgFilename, $outputFilename, $format)
+    {
+        $cmd = "/usr/bin/rsvg-convert -f {$format}";
+        if ($this->width) {
+            $cmd .= " -w {$this->width}";
+        }
+        if ($this->height) {
+            $cmd .= " -h {$this->height}";
+        }
+
+        $cmd .= " {$svgFilename} > {$outputFilename}";
+
+        return shell_exec($cmd);
+    }
+
+    /**
+     * @param $width
+     * @param $height
+     * @return $this
+     */
+    public function setSize($width, $height): self
+    {
+        $this->width = $width;
+        $this->height = $height;
+
+        return $this;
+    }
+}
